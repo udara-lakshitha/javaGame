f649ea03bd11bbfbc3f10fd8ea15d98285f522f4
Udara Lakshitha <56027523+udara-lakshitha@users.noreply.github.com>
2019-11-25 21:52:35 +0530

    Add files via upload

---
+++ Game/src/brickBreaker/Main.java
@@ -0,0 +1,20 @@
+package brickBreaker;
+
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Gameplay gamePlay = new Gameplay();
+		JFrame obj = new JFrame();
+		obj.setBounds(10, 10, 700, 600);
+		obj.setTitle("Breakout Ball");
+		obj.setResizable(false);
+		obj.setVisible(true);
+		obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		obj.add(gamePlay);
+
+	}
+
+}
